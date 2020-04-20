6458d75a5f345789f8f9c6281a0e11512de7240b
Jonas Belouadi <bigjonas96@gmail.com>
2020-04-20 21:08:59 +0200

    Don't unnecessarily clone the commands table

--- parser.lua
+++ parser.lua
@@ -1,18 +1,17 @@
-local gears   = require("gears")
-
-local function match(sequence, pattern)
-  local capture      = string.match(sequence, '^' .. pattern[1])
-  local sub_sequence = string.gsub(sequence, '^' .. pattern[1], '')
+local function match(sequence, pattern, index)
+  index              = index or 1
+  local capture      = string.match(sequence, '^' .. pattern[index])
+  local sub_sequence = string.gsub(sequence, '^' .. pattern[index], '')
 
   if capture then
-    table.remove(pattern, 1)
-    if #sub_sequence == 0 and #pattern == 0 then
+    if #sub_sequence == 0 and #pattern == index then
       return {capture}, true, true
-    elseif #sub_sequence == 0 and #pattern > 0 then
+    elseif #sub_sequence == 0 and #pattern > index then
       return {capture}, true, false
-    elseif #sub_sequence > 0 and #pattern > 0 then
-      local captures, valid, finished = match(sub_sequence, pattern)
-      return gears.table.merge({capture}, captures), valid, finished
+    elseif #sub_sequence > 0 and #pattern > index then
+      local captures, valid, finished = match(sub_sequence, pattern, index + 1)
+      table.insert(captures, 1, capture)
+      return captures, valid, finished
     end
   end
   return {}, false, false
@@ -21,7 +20,7 @@ end
 local function parse(sequence, commands)
   local should_break = true
   for _, command in ipairs(commands) do
-    local captures, valid, finished = match(sequence, gears.table.clone(command.pattern))
+    local captures, valid, finished = match(sequence, command.pattern)
 
     if finished then
       command:handler(table.unpack(captures))
@@ -37,6 +36,5 @@ local function parse(sequence, commands)
 
   return false
 end
-
 
 return {parse = parse}
